e42098354bcd432d4888899c04d3b4769773b628
baekg6 <baekg.dev@gmail.com>
2022-11-30 21:35:40 +0900

    Design: 전역 스타일 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import reset from 'styled-reset';
 import HomePage from './pages/HomePage/HomePage';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 
+const GlobalStyle = createGlobalStyle`
+    ${reset}
+    *{
+        box-sizing:border-box;
+        font-family:'Spoqa Han Sans Neo', sans-serif;
+    }
+`;
+
 function App() {
     return (
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/product/:id" element={<ProductDetail />} />
-            </Routes>
-        </BrowserRouter>
+        <>
+            <GlobalStyle />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                    <Route path="/product/:id" element={<ProductDetail />} />
+                </Routes>
+            </BrowserRouter>
+        </>
     );
 }
 export default App;
