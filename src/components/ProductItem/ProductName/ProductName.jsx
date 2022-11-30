a6051899fa5f026dae068aab4774bc77cfbb9662
baekg6 <baekg.dev@gmail.com>
2022-11-30 21:30:03 +0900

    Feat: ProductName 컴포넌트

--- src/components/ProductItem/ProductName/ProductName.jsx
+++ src/components/ProductItem/ProductName/ProductName.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const ProductNameCont = styled.strong`
+    font-size: 18px;
+    line-height: 22px;
+    width: 338px;
+    margin: 20px 0 10px;
+`;
 
 export default function ProductName({ name }) {
-    return <div>{name}</div>;
+    return <ProductNameCont>{name}</ProductNameCont>;
 }
